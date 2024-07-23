560f2792aa1f2c8231671f9fbf09f79804a84ca6
Matias Leal <matiasleal96@hotmail.com>
2024-07-22 21:02:09 -0300

    Unused funtion un database.py deleted

--- login-doc/database.py
+++ login-doc/database.py
@@ -1,4 +1,4 @@
-from schemes import *
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -6,24 +6,3 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine("sqlite:///my_database.db", echo=True)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-    else:
-        return None
-
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-def decode_token(token):
-    user = get_user(users_db,token)
-    return user
-
-
-
-
